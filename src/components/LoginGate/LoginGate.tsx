b29ddaaa76bce653ae2e3a66b85d587fe396ee10
rafael <rafaelbordoni00@gmail.com>
2022-11-28 17:56:15 -0300

    fix: LoginGate

--- src/components/LoginGate/LoginGate.tsx
+++ src/components/LoginGate/LoginGate.tsx
@@ -1,8 +1,8 @@
-import React, { useState, useEffect } from 'react';
+import { useState, useEffect } from 'react';
 import { useToken } from 'hooks';
 import { useSignOut } from 'api';
 
-export function LoginGate(props: { node: React.ReactNode }) {
+export function LoginGate(props: { node: JSX.Element }) {
   const [ready, setReady] = useState<boolean>(false);
   const token = useToken();
   const signOut = useSignOut();
